404efa743778e635552d0bf14773ac2fbb48d21a
sg-nta <an.nt204865@sis.hust.edu.vn>
2023-09-20 20:58:47 +0700

    increase num segments

--- models/emb_proj.py
+++ models/emb_proj.py
@@ -31,7 +31,7 @@ class MLP(nn.Module):
     def forward(self, x):
         #x.requires_grad = True
         x = checkpoint_sequential(functions=self.module_list, 
-                                  segments=2, 
+                                  segments=4, 
                                   input=x)
         return x
 
