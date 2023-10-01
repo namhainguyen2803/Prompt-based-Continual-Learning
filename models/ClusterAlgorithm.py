9b8c28508ecfba71b5211d99b515509193eb5a21
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2023-10-01 21:23:31 +0700

    hello

--- models/ClusterAlgorithm.py
+++ models/ClusterAlgorithm.py
@@ -6,48 +6,82 @@ import numpy as np
 
 class KMeans():
 
-    def __init__(self, num_classes=5, max_iter=1000):
+    def __init__(self, num_classes=5, max_iter=1000, init_times=50):
 
         self._centroids = None
         self.num_classes = num_classes
         self.max_iter = max_iter
+        self.init_times = init_times
         self._tol = 1e-8
 
     def get_centroids(self):
         return self._centroids
 
     def _get_initial_centroids(self, features_matrix):
-        permute_tensor_range = torch.randperm(features_matrix.shape[0])[:self.num_classes]
-        centroids = features_matrix[permute_tensor_range]
-        self._centroids = centroids.reshape(self.num_classes, features_matrix.shape[1])
+        if self.init_times > 1:
+            optimal_init = None
+            optimal_init_purity = 1e9
+            for i in range(self.init_times):
+                permute_tensor_range = torch.randperm(features_matrix.shape[0])[:self.num_classes]
+                centroids = features_matrix[permute_tensor_range]
+                centroids = centroids.reshape(self.num_classes, features_matrix.shape[1])
+                purity = self.loss_function(features_matrix, centroids)
+                if purity < optimal_init_purity:
+                    optimal_init_purity = purity
+                    optimal_init = centroids
+            return optimal_init
+        else:
+            permute_tensor_range = torch.randperm(features_matrix.shape[0])[:self.num_classes]
+            centroids = features_matrix[permute_tensor_range]
+            centroids = centroids.reshape(self.num_classes, features_matrix.shape[1])
+            return centroids
 
     def _calculate_cluster_centers(self, features_matrix, cluster_labels):
         for i in range(0, self.num_classes):
             data = features_matrix[cluster_labels == i]
             self._centroids[i, :] = torch.mean(data, dim=0)
 
+    def _calculate_cluster_label(self, features_matrix, centroids=None):
+        centroids = self._centroids if centroids is None else centroids
+        distance = self._get_euclidean_distances(centroids=centroids, datapoints=features_matrix)
+        cluster_labels = torch.argmin(distance, dim=0)
+        return cluster_labels
+
     def _get_euclidean_distances(self, centroids, datapoints):
         if centroids.ndim == 1:
             centroids = centroids.unqueeze(0)
-
         centroids = centroids.unsqueeze(1)
         num_centroids = centroids.shape[0]
         num_datapoints = datapoints.shape[0]
         distance = centroids - datapoints
         distance = torch.linalg.norm(distance, dim=-1)
         assert distance.shape == (num_centroids, num_datapoints), "distance.shape != (num_centroids, num_datapoints)"
-
         return distance
 
-    def fit(self, features_matrix):
-        self._get_initial_centroids(features_matrix)
+    def loss_function(self, features_matrix, centroids=None):
+        purity = 0
+        centroids = self._centroids if centroids is None else centroids
+        distance = self._get_euclidean_distances(centroids=centroids, datapoints=features_matrix)
+        cluster_labels = torch.argmin(distance, dim=0)
+        for i in range(0, self.num_classes):
+            data = features_matrix[cluster_labels == i]
+            purity += torch.norm(data - centroids[i, :], p=2, dim=1).mean()
+        return purity
+
+    def fit(self, features_matrix, centroids=None):
+        if centroids is None:
+            self._centroids = self._get_initial_centroids(features_matrix)
+        else:
+            self._centroids = centroids
+
         for i in range(0, self.max_iter):
             old_centroids = copy.deepcopy(self._centroids)
-            distance = self._get_euclidean_distances(centroids=self._centroids, datapoints=features_matrix)
-            cluster_labels = torch.argmin(distance, dim=0)
+            cluster_labels = self._calculate_cluster_label(features_matrix, self._centroids)
             self._calculate_cluster_centers(features_matrix, cluster_labels)
-            if torch.max(torch.abs((self._centroids - old_centroids) / old_centroids)) < self._tol:
+            if torch.norm((old_centroids - self._centroids), dim=1).max() < self._tol:
                 break
+        purity = self.loss_function(features_matrix, self._centroids)
+        return purity
 
     def predict_labels(self, features_matrix):
 
@@ -60,48 +94,16 @@ class KMeans():
         return cluster_labels
 
 
-class KMeans2():
+def fit_kmeans_many_times(features_matrix, fit_times=50, init_times_for_each_kmeans=1):
+    optimal_loss = 1e9
+    optimal_model = None
+    for i in range(fit_times):
+        model = KMeans(init_times=init_times_for_each_kmeans)
+        loss = model.fit(features_matrix)
+        if loss < optimal_loss:
+            optimal_loss = loss
+            optimal_model = model
+        else:
+            del model
+    return optimal_model, optimal_loss
 
-    def __init__(self, num_classes=5, max_iter=50):
-        self.init_times = max_iter
-        self.n_centers = num_classes
-        self.min_delta = 1e-3
-        self._centroids = None
-
-    def fit(self, x):
-        if len(x.size()) == 3:
-            x = x.squeeze(1)
-        x_min, x_max = x.min(), x.max()
-        x = (x - x_min) / (x_max - x_min)
-
-        min_cost = np.inf
-        center = None
-        for i in range(self.init_times):
-            tmp_center = x[np.random.choice(np.arange(x.shape[0]), size=self.n_centers, replace=False), ...]
-            l2_dis = torch.norm((x.unsqueeze(1).repeat(1, self.n_centers, 1) - tmp_center), p=2, dim=2)
-            l2_cls = torch.argmin(l2_dis, dim=1)
-
-            cost = 0
-            for c in range(self.n_centers):
-                cost += torch.norm(x[l2_cls == c] - tmp_center[c], p=2, dim=1).mean()
-
-            if cost < min_cost:
-                min_cost = cost
-                center = tmp_center
-
-        delta = np.inf
-
-        while delta > self.min_delta:
-            l2_dis = torch.norm((x.unsqueeze(1).repeat(1, self.n_centers, 1) - center), p=2, dim=2)
-            l2_cls = torch.argmin(l2_dis, dim=1)
-            center_old = center.clone()
-
-            for c in range(self.n_centers):
-                center[c] = x[l2_cls == c].mean(dim=0)
-
-            delta = torch.norm((center_old - center), dim=1).max()
-
-        self._centroids = center.unsqueeze(0) * (x_max - x_min) + x_min
-
-    def get_centroids(self):
-        return self._centroids
--- models/LearnDistribution.py
+++ models/LearnDistribution.py
@@ -1,3 +1,5 @@
+from models.ClusterAlgorithm import KMeans, fit_kmeans_many_times
+
 import copy
 from abc import ABC, abstractmethod
 
@@ -7,7 +9,6 @@ import torch.nn as nn
 from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.distributions.categorical import Categorical
 from math import pi
-from models.ClusterAlgorithm import KMeans
 
 
 class AbstractLearningDistributionMethod(ABC):
@@ -55,8 +56,7 @@ class Gaussian(AbstractLearningDistributionMethod):
 
 class MixtureGaussian(AbstractLearningDistributionMethod):
 
-    def __init__(self, num_clusters=5, *args, **kwargs):
-        super(MixtureGaussian, self).__init__(*args, **kwargs)
+    def __init__(self, num_clusters=5):
         self.num_clusters = num_clusters
         self.EPS = 1e-6
         self.mu = None
@@ -64,10 +64,12 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         self.sigma = None
         self.gaussian_list = list()
         self._init_mu = None
+        self.diff_threshold = 1e-5
 
     def _initialize_set_mean(self, data):
-        k_means = KMeans(num_classes=self.num_clusters, max_iter=1000)
-        k_means.fit(data)
+        k_means, loss = fit_kmeans_many_times(features_matrix=data, fit_times=50)
+        # k_means = KMeans(num_classes=self.num_clusters, max_iter=1000)
+        # k_means.fit(data)
         return k_means.get_centroids()
 
     def _initialize_parameters(self, data):
@@ -78,12 +80,13 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         mu = self._initialize_set_mean(data)  # (num_clusters, num_features)
         self._init_mu = copy.deepcopy(mu)
         sigma = torch.eye(num_features).unsqueeze(0).repeat(self.num_clusters, 1, 1)
-
         return mu, sigma, pi
 
-    def _calculate_difference_prior_mu_and_posterior_mu(self):
-        mu_diff = self.mu - self._init_mu
-        print(f"Relative difference between prior mu and posterior mu: {torch.max(torch.abs(mu_diff))}")
+    def _log_det(self, var):
+        log_det = torch.empty(size=(self.num_clusters,))
+        for k in range(self.num_clusters):
+            log_det[k] = 2 * torch.log(torch.diagonal(torch.linalg.cholesky(var[k]))).sum()
+        return log_det
 
     def log_likelihood(self, data):
         """
@@ -99,20 +102,20 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         num_instances = data.shape[0]
         num_features = data.shape[1]
         x_minus_mu_times_sigma = torch.zeros(num_instances, self.num_clusters, num_features)
-        list_det = torch.linalg.det(self.sigma)  # (num_cluster)
         sigma_inv = torch.linalg.inv(self.sigma)  # (num_cluster, num_features, num_features)
         x_minus_mu = data.unsqueeze(1) - self.mu.unsqueeze(0)  # (num_instance, num_cluster, num_features)
         for c in range(self.num_clusters):
             x_minus_mu_times_sigma[:, c, :] = torch.mm(x_minus_mu[:, c, :], sigma_inv[c, :, :])
             # (num_instance, num_features) @ (num_features, num_features) = (num_instance, num_features)
         assert x_minus_mu_times_sigma.shape == x_minus_mu.shape
-        x_minus_mu_times_sigma_times_x_minus_mu = torch.sum(x_minus_mu_times_sigma * x_minus_mu, dim=-1)
-        assert x_minus_mu_times_sigma_times_x_minus_mu.shape == (num_instances, self.num_clusters)
+        log_manhattan_dist = torch.sum(x_minus_mu_times_sigma * x_minus_mu, dim=-1)
+        assert log_manhattan_dist.shape == (num_instances, self.num_clusters)
 
-        log_det = torch.log(list_det + self.EPS).reshape(1, -1)
-        log_manhattan_dist = x_minus_mu_times_sigma_times_x_minus_mu
+        log_det = self._log_det(self.sigma).reshape(1, -1)
         log_pi = num_features * np.log(2 * torch.pi)
         log_prob = (-0.5) * (log_manhattan_dist + log_det + log_pi)
+        check_tensor_nan(log_prob, "log_prob")
+
         return log_prob
 
     def _calculate_prob_z_given_x(self, log_prob_x_and_z):
@@ -128,9 +131,13 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         """
         max_element = torch.max(log_prob_x_and_z)
         normalized_log_prob_x_and_z = log_prob_x_and_z - max_element
+
         exp_normalized_log_prob_x_and_z = torch.exp(normalized_log_prob_x_and_z) + self.EPS
+        check_tensor_nan(exp_normalized_log_prob_x_and_z, "exp_normalized_log_prob_x_and_z")
+        check_tensor_nan(torch.sum(exp_normalized_log_prob_x_and_z, dim=1), "exp_normalized_log_prob_x_and_z (2)")
         prob_z_given_x = exp_normalized_log_prob_x_and_z / torch.sum(exp_normalized_log_prob_x_and_z, dim=1).reshape(-1,
                                                                                                                      1)
+        check_tensor_nan(prob_z_given_x, "prob_z_given_x")
         return prob_z_given_x
 
     def log_joint_distribution(self, data):
@@ -144,9 +151,10 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         -------
         log_prob_x_and_z        torch.Tensor (num_instances, num_clusters)
         """
-        log_p_x_given_z = self.log_likelihood(data)
+        log_p_x_given_z = self.log_likelihood(data)  # (num_instance, num_clusters)
         log_pi = torch.log(self.pi + self.EPS).reshape(1, -1)
         log_prob_x_and_z = log_p_x_given_z + log_pi
+        check_tensor_nan(log_prob_x_and_z, "log_prob_x_and_z")
         return log_prob_x_and_z
 
     def log_marginal_distribution(self, data):
@@ -160,11 +168,15 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         -------
         log_prob_x        torch.Tensor (num_instances,)
         """
-        return torch.sum(self.log_joint_distribution(data), dim=-1)
+        num_instances = data.shape[0]
+        log_joint_prob = self.log_joint_distribution(data)
+        log_marginal_prob = torch.logsumexp(log_joint_prob, dim=1)
+        assert log_marginal_prob.shape == (num_instances,)
+        return log_marginal_prob
 
     def posterior_distribution(self, data):
         """
-        Calculate probability of each class given data p(class|data)
+        Calculate probability of each class given data
         Parameters
         ----------
         data             torch.Tensor (num_instances, num_features)
@@ -174,9 +186,10 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         prob_z_given_x    torch.Tensor (num_instances, num_clusters)
         """
         log_p_x_given_z = self.log_likelihood(data)
-        log_pi = torch.log(self.pi + self.EPS).reshape(1, -1)
+        log_pi = torch.log(self.pi).reshape(1, -1)
         log_prob_x_and_z = log_p_x_given_z + log_pi
         prob_z_given_x = self._calculate_prob_z_given_x(log_prob_x_and_z)
+
         return prob_z_given_x
 
     def _update_parameter(self, prob_z_given_x, data):
@@ -196,29 +209,22 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
         num_instances = data.shape[0]
         num_features = data.shape[1]
 
-        unormalized_pi = torch.sum(prob_z_given_x, dim=0)
+        unormalized_pi = torch.sum(prob_z_given_x, dim=0) + self.EPS
         mu = torch.sum(prob_z_given_x.unsqueeze(-1) * data.unsqueeze(1), dim=0) / (
-                    unormalized_pi.reshape(-1, 1) + self.EPS)
+            unormalized_pi.reshape(-1, 1))
 
         x_minus_mu = data.unsqueeze(1) - mu.unsqueeze(0)
 
-        # OPTION 1
-        # resp = prob_z_given_x.unsqueeze(-1)
-        # var_2 = torch.sum(x_minus_mu.unsqueeze(-1).matmul(x_minus_mu.unsqueeze(-2)) * resp.unsqueeze(-1), dim=0,
-        #                 keepdim=True) / torch.sum(resp, dim=0, keepdim=True).unsqueeze(-1)
-        # var_2 = var_2.squeeze(0)
-
-        # OPTION 2
-        var = torch.zeros(self.num_clusters, num_features, num_features)
+        sigma = torch.zeros(self.num_clusters, num_features, num_features)
         for c in range(self.num_clusters):
-            cac = x_minus_mu[:, c, :] # (num_instances, num_features)
+            cac = x_minus_mu[:, c, :]  # (num_instances, num_features)
             p = prob_z_given_x[:, c].reshape(-1, 1)
-            var[c,:,:] = torch.mm(cac.transpose(-2, -1), p*cac)
+            sigma[c, :, :] = torch.mm(cac.transpose(-2, -1), p * cac)
+        sigma = sigma / (unormalized_pi.reshape(-1, 1, 1))
 
-        var = var / (unormalized_pi.reshape(-1, 1, 1))
         pi = unormalized_pi / num_instances
 
-        return pi, mu, var
+        return pi, mu, sigma
 
     def learn_distribution(self, data, epoch=200):
         num_instances = data.shape[0]
@@ -227,25 +233,30 @@ class MixtureGaussian(AbstractLearningDistributionMethod):
 
         it = 0
         old_loss = -1e9
+
         while it < epoch:
             it += 1
             # E step
             prob_z_given_x = self.posterior_distribution(data)
 
-            pi, mu, var = self._update_parameter(prob_z_given_x, data)
+            pi, mu, sigma = self._update_parameter(prob_z_given_x, data)
 
             self.pi = pi
             self.mu = mu
-            self.var = var
+            self.sigma = sigma
 
             assert self.mu.shape == (self.num_clusters, num_features)
             assert self.pi.shape == (self.num_clusters,)
             assert self.sigma.shape == (self.num_clusters, num_features, num_features)
 
-            log_p_x = torch.sum(self.log_marginal_distribution(data))
-            print(f"Loss function: {log_p_x}. Check if new logP(x) > old logP(x): {log_p_x > old_loss}")
-            old_loss = log_p_x
+            log_p_x = torch.mean(self.log_marginal_distribution(data))
+            diff_gap = log_p_x - old_loss
 
+            print(f"Iteration {it}. Loss function: {log_p_x}. Check if new logP(x) > old logP(x): {diff_gap}")
+            if diff_gap < self.diff_threshold:
+                break
+
+            old_loss = log_p_x
 
         for j in range(self.num_clusters):
             gaussian = MultivariateNormal(loc=self.mu[j, :], covariance_matrix=self.sigma[j, :, :])
@@ -269,3 +280,15 @@ def get_learning_distribution_model(model_type="gaussian"):
         return Gaussian()
     elif model_type == "gmm":
         return MixtureGaussian()
+
+
+def check_symmetric(mat, num):
+    # print(mat == mat.transpose(-2, -1))
+    res = torch.sum(mat == mat.transpose(-2, -1)) / mat.numel()
+    print(f"Check symmetric in {num}: {res}")
+
+
+def check_tensor_nan(tensor, tensor_name="a"):
+    has_nan = torch.isnan(tensor).any().item()
+    if has_nan:
+        raise TypeError(f"Tensor {tensor_name} is nan.")
