0cc39bb37a3cb2b2cfb41c460e96553078f1e165
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2023-10-08 01:27:16 +0700

    hello

--- learners/prompt.py
+++ learners/prompt.py
@@ -758,10 +758,10 @@ class GaussianFeaturePrompt(Prompt):
 
         # step
         self.optimizer.zero_grad()
-        # self.label_embedding_optim.zero_grad()
+        self.label_embedding_optim.zero_grad()
         total_loss.backward()
         self.optimizer.step()
-        # self.label_embedding_optim.step()
+        self.label_embedding_optim.step()
 
         return total_loss.detach(), gaussian_penalty.detach(), logit
 
@@ -794,9 +794,9 @@ class GaussianFeaturePrompt(Prompt):
         task_info = self.tasks[task]
         num_classes = len(task_info)
         self.label_embedding = nn.Parameter(data=torch.randn(num_classes, self.model.feature_dim, device='cuda'),
-                                            requires_grad=False)
+                                            requires_grad=True)
         # self.label_embedding = nn.Linear(1, self.model.feature_dim).cuda()
-        # self.label_embedding_optim = torch.optim.Adam(lr=0.0005, params=[self.label_embedding])
+        self.label_embedding_optim = torch.optim.Adam(lr=0.0005, params=[self.label_embedding])
 
     def create_validation_classifier(self, linear_model=True):
         feature_dim = self.model.feature_dim
