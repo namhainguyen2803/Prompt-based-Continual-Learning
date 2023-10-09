b6318ed9e29793f86025c0c0c6853e7add6cf07b
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2023-10-09 13:16:55 +0700

    experiment

--- learners/prompt.py
+++ learners/prompt.py
@@ -756,7 +756,7 @@ class GaussianFeaturePrompt(Prompt):
         normalized_label_embedding = nn.functional.normalize(self.label_embedding, dim=-1)
 
         penalty = torch.sum(torch.matmul(normalized_mean, normalized_label_embedding.T)) + \
-                  torch.sum(torch.diag(normalized_mean, normalized_mean.T))
+                  torch.sum(torch.diagonal(normalized_mean, normalized_mean.T))
 
         gaussian_penalty = torch.mean(torch.sum((feature - pseudo_mean) ** 2, dim=1)) - \
                            0.1 * penalty
