40621dd12c262e4ef3ccbee6d634e43dd4b4c012
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2023-10-13 22:29:37 +0700

    experiment

--- learners/prompt.py
+++ learners/prompt.py
@@ -900,8 +900,10 @@ class GaussianFeaturePrompt(Prompt):
 
             flatten_possible_class_id = possible_class_id.reshape(-1, 1).squeeze(-1).cpu()
             selected_score = score_likelihood[
-                torch.arange(start=0, end=B).reshape(-1, 1).repeat(1, top_k).reshape(-1, 1).squeeze(-1).to(torch.int32),
-                flatten_possible_class_id.to(torch.int32)].reshape(B, -1)
+                # torch.arange(start=0, end=B).reshape(-1, 1).repeat(1, top_k).reshape(-1, 1).squeeze(-1).to(torch.int32),
+                range(B * top_k),
+                flatten_possible_class_id.to(torch.int32)
+            ].reshape(B, -1)
 
             assert selected_score.shape == (B, top_k)
 
